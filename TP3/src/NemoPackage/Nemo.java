1762982ebbd5e221058b7f20e799c1eae47f30e6
naomicouriel <71277206+naomicouriel@users.noreply.github.com>
2023-10-15 18:49:34 -0300

    Implementación con if
    Queda aplicar el polimorfismo con juego de roles, y los tests de a pares o más de comandos

--- TP3/src/NemoPackage/Nemo.java
+++ TP3/src/NemoPackage/Nemo.java
@@ -1,11 +1,18 @@
 package NemoPackage;
 
 public class Nemo {
-    public String south;
-    public int depth = 0;
+    public static String CannotReleaseCapsuleInDeeperLevels = "No se puede liberar la cápsula en otro nivel";
+	public int depth = 1;
     public String direction = "South";
     
     public Posicion posicion = new Posicion(0, 0);
+    public int posicionX = 0;
+    public int posicionY = 0;
+    public boolean capsuleIsLiberated = false;
+    
+    public void setDirection(String directionToSet) {
+    	direction = directionToSet;
+    }
 
     public int getPositionX(){
         return posicion.getX();   
@@ -30,7 +37,7 @@ public class Nemo {
     public int moveY(String string) {
         return posicion.getY();
     }
-
+/*
     public int moveDepth(char c) {
         if (c == 'd') {
             depth += 1;
@@ -63,6 +70,97 @@ public class Nemo {
                     break;
             }
         }
+          if (c == 'r') {
+        	  switch(direction) {
+        	  case "South":
+                  direction = "West";
+                  break;
+              case "North":
+                  direction = "East";
+                  break;
+              case "West":
+                  direction = "North";
+                  break;
+              case "East":
+                  direction = "South";
+                  break;
+        	  }
+          }
+        
         return direction;        
     }
+
+	public int moveFordward(char c) {
+		Posicion newposicion = new Posicion(1, 0);
+		return newposicion.getX();
+	}
+
+	public void releaseCapsule(char c) throws Exception {
+		if (depth == 0 || depth == 1) {
+			capsuleIsLiberated = true;
+		}
+		else{
+			throw new Exception("No se puede liberar la cápsula en otro nivel");
+			//destroy nemo
+		}
+	}
+	*/
+	public void processCommand(String command) throws Exception {
+		if (command == "d") {
+            depth += 1;
+        }
+        if (command == "u") {
+            if (getDepth() == 0) {
+                depth = 0;
+            }
+            else {
+                depth -= 1;
+            }
+        }
+        if (command == "l") {
+            switch (direction) {
+                case "South":
+                    direction = "East";
+                    break;
+                case "North":
+                    direction = "West";
+                    break;
+                case "West":
+                    direction = "South";
+                    break;
+                case "East":
+                    direction = "North";
+                    break;
+            }
+        }
+          if (command == "r") {
+        	  switch(direction) {
+        	  case "South":
+                  direction = "West";
+                  break;
+              case "North":
+                  direction = "East";
+                  break;
+              case "West":
+                  direction = "North";
+                  break;
+              case "East":
+                  direction = "South";
+                  break;
+        	  }
+          }
+          if (command == "f") {
+      		//Posicion newposicion = new Posicion(1, 0);
+        	  posicionX += 1;
+          }
+          if(command == "m") {
+        	  if (depth == 0 || depth == 1) {
+      			capsuleIsLiberated = true;
+      		}
+      		else{
+      			throw new RuntimeException(CannotReleaseCapsuleInDeeperLevels);
+      			//destroy nemo
+      		}
+          }
+	}
 }
--- TP3/src/NemoPackage/NemoTest.java
+++ TP3/src/NemoPackage/NemoTest.java
@@ -8,36 +8,141 @@ import org.junit.jupiter.api.Test;
 
 public class NemoTest {
 	
-	@Test public void test00() {
+	@Test public void test00NemoIsCorrectlyInitialized() {
 		Nemo nemo = new Nemo();
 		assertEquals(nemo.getPositionX(), 0);
 		assertEquals(nemo.getPositionY(), 0);
-		assertEquals(nemo.getDepth(), 0);
+		assertEquals(nemo.getDepth(), 1);
 		assertEquals(nemo.getDirection(), "South");
 	}
-	@Test public void test01() {
+	@Test public void test01NemoStaysInTheSamePositionWhenCommandIsEmpty() {
 		Nemo nemo = new Nemo();
 		assertEquals( nemo.getPositionX(), nemo.moveX("") );
 		assertEquals( nemo.getPositionY(), nemo.moveY("") ); //move me retorna la position
 		//Si le paso un string vacío que no se mueva
 	}
-	@Test public void test02() {
+	@Test public void test02CommandDNemoDescendsOneUnitInLevelOneOfImmersion() throws Exception {
 		//le paso un char 'd'
 		Nemo nemo = new Nemo();
-		nemo.moveDepth('d');
-		assertEquals(1, nemo.getDepth());
+		nemo.processCommand("d");
+		assertEquals(2, nemo.getDepth());
 	}
 	
-	@Test public void test03() {
+	@Test public void test03CommandUNemoAscendsOneUnit() throws Exception {
 		//le paso un char 'u'
 		Nemo nemo = new Nemo();
-		nemo.moveDepth('u');
+		nemo.processCommand("u");
 		assertEquals(0, nemo.getDepth());
 	}
 	
-	@Test public void test04() {
+	@Test public void test04CommandLNemoTurnsLeft() throws Exception {
 		Nemo nemo = new Nemo();
-		nemo.rotate('l');
-		assertEquals(nemo.getDirection(), "East");
+		nemo.processCommand("l");
+		assertEquals( "East", nemo.getDirection());
 	}
+	
+	@Test public void test05CommandRNemoTurnsRight() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.processCommand("r");
+		assertEquals( "West", nemo.getDirection());
+	}
+	@Test public void test06CommandFNemoMovesForwardOneUnit() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.processCommand("f");
+		assertEquals(1, nemo.posicionX);
+	}
+	@Test public void test07CommandMNemoReleasesCapsule() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.processCommand("m");
+		assertTrue(nemo.capsuleIsLiberated);
+	}
+	@Test public void test08DoesNotFailWhenGoingDownTooMuch() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.processCommand("d");
+		nemo.processCommand("d");
+		nemo.processCommand("d");
+		nemo.processCommand("d");
+		assertEquals(5, nemo.getDepth());
+	}
+	@Test public void test09DoesNotFailWhenGoingUpTooMuchAndStaysInSurface() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.processCommand("u");
+		nemo.processCommand("u");
+		nemo.processCommand("u");
+		nemo.processCommand("u");
+		assertEquals(0, nemo.getDepth());
+	}
+	
+	@Test public void test10WhenNemoIsFacingNorthTurningLeftFacesWest() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.setDirection("North");
+		nemo.processCommand("l");
+		assertEquals("West", nemo.getDirection());
+	}
+	@Test public void test11WhenNemoIsFacingWestTurningLeftFacesSouth() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.setDirection("West");
+		nemo.processCommand("l");
+		assertEquals("South", nemo.getDirection());
+	}
+	@Test public void test12WhenNemoIsFacingEastTurningLeftFacesNorth() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.setDirection("East");
+		nemo.processCommand("l");
+		assertEquals("North", nemo.getDirection());
+	}
+	@Test public void test13WhenNemoIsFacingNorthTurningRightFacesEast() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.setDirection("North");
+		nemo.processCommand("r");
+		assertEquals("East", nemo.getDirection());
+	}
+	@Test public void test14WhenNemoIsFacingWestTurningRightFacesNorth() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.setDirection("West");
+		nemo.processCommand("r");
+		assertEquals("North", nemo.getDirection());
+	}
+	@Test public void test15WhenNemoIsFacingEastTurningRightFacesSouth() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.setDirection("East");
+		nemo.processCommand("r");
+		assertEquals("South", nemo.getDirection());
+	}
+	@Test public void test16RotatesLeftAsMuchAsAsked() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.processCommand("l");
+		nemo.processCommand("l");
+		nemo.processCommand("l");
+		nemo.processCommand("l");
+		assertEquals("South", nemo.getDirection());
+	}
+	@Test public void test17RotatesRightAsMuchAsAsked() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.processCommand("r");
+		nemo.processCommand("r");
+		nemo.processCommand("r");
+		nemo.processCommand("r");
+		assertEquals("South", nemo.getDirection());
+	}
+	@Test public void test18CannotReleaseCapsuleIfLevelDeeperThanOne() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.processCommand("d");
+		assertEquals( Nemo.CannotReleaseCapsuleInDeeperLevels,
+				  assertThrows( RuntimeException.class, () -> nemo.processCommand("m") ).getMessage() );
+	}
+	
+	@Test public void test19CapsuleReleasesWhenNemoIsOnSurface() throws Exception {
+		Nemo nemo = new Nemo();
+		nemo.processCommand("m");
+		assertTrue(nemo.capsuleIsLiberated);
+	}
+
+	/*que pasa si le insistis mucho con bajar
+	que pasa si le insistis mucho con subir
+	para donde se mueve si esta faceando para cada lado
+	que rote tantasveces para la izq der como le pidas
+	que puedas lanzar la capsula en el primer nivel o superficie y despues no
+	combinaciones de a pares o triplets
+	*/
 }
